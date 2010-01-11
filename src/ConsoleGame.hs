4c5bf63de5be5ea72c41e41c0f42748e2cab2caf
Eric Mertens <emertens@eric-mertenss-macbook-pro.local>
2010-01-10 20:14:06 -0800

    Improve drawing code and cursor behavior

--- src/ConsoleGame.hs
+++ src/ConsoleGame.hs
@@ -21,6 +21,8 @@ main = do
         $ newInterface g
   shutdown vty
 
+-- | 'run' is the main event-loop for the game. It alternates
+--   reading user input and drawing the interface.
 run :: Vty -> Game -> Interface -> IO ()
 run vty game s
  | emptyGame game = return ()
@@ -30,40 +32,128 @@ run vty game s
   cmd <- handleInput vty
   let simple f = Just (game, f s')
   traverse_ (uncurry (run vty)) $ case cmd of
-        Deal        -> checkNoSets game s
+        Deal        -> checkNoSets game s'
         DeleteLast  -> simple $ initSelection
                               . clearMessage
         Hint        -> simple $ giveHint game
         Move dir    -> simple $ updateControl $ moveCur dir $ length
                               $ tableau game
         Quit        -> Nothing
-        Select      -> Just (select game s')
+        Select      -> select game s'
 
-select :: Game -> Interface -> (Game, Interface)
+-- | 'select' performs an event on the interface based on the currently
+--   focused control.
+select :: Game -> Interface -> Maybe (Game, Interface)
 select game s = case iControl s of
- CardButton i ->
-  case index i (tableau game) of
-   Just t | isSelected t s -> (game, updateSelection (delete t)
-                                   . clearMessage
-                                   $ s)
-          | otherwise    -> addCard t game s
-   Nothing               -> (game, setControl (CardButton 0) s)
+ CardButton i -> case index i (tableau game) of
+   Just t | isSelected t s      -> Just (game, updateSelection (delete t)
+                                             . clearMessage
+                                             $ s)
+          | otherwise           -> addCard t game s
+   Nothing                      -> Just (game, setControl (CardButton 0)
+                                             $ s)
 
-addCard :: Card -> Game -> Interface -> (Game, Interface)
+-- | 'addCard' attempts to add a new card to the selection and handles
+--   checking for a valid set when the selection becomes full.
+addCard :: Card -> Game -> Interface -> Maybe (Game, Interface)
 addCard card0 game s = case iSelection s of
-  (_:_:_:_)      -> (game, setMessage "Selection full" s)
+  (_:_:_:_)      -> Just (game, setMessage "Selection full" s)
   [card1, card2] -> checkSet card0 card1 card2 game . appendSelection card0
                                                     . clearMessage
                                                     $ s
-  _              -> (game, appendSelection card0
-                         . clearMessage
-                         $ s)
+  _              -> Just (game, appendSelection card0
+                              . clearMessage
+                              $ s)
 
+-- | 'setGame' updates the 'Interface' based on the current 'Game' state.
 setGame :: Game -> Interface -> Interface
 setGame game = setTableau (tableau game)
              . setRemaining (deckSize game)
 
--- Interface manipulation functions
+moveCur :: Direction -> Int -> CurrentControl -> CurrentControl
+moveCur dir n (CardButton i) = CardButton i2
+ where
+  (row,col) = i `divMod` cols
+
+  (row1, col1) = case dir of
+    GoUp        -> (row - 1, col)
+    GoDown      -> (row + 1, col)
+    GoLeft      -> (row, col - 1)
+    GoRight     -> (row, col + 1)
+
+  i2 = case dir of
+    _ | toI (row1, col1) < n
+                -> toI (row1, col1)
+    GoUp        -> toI (-2, col1)
+    GoDown      -> toI (0, col1)
+    GoLeft      -> toI (row1, n-1)
+    GoRight     -> toI (row1, 0)
+
+  toI (r,c) = r `mod` rows * cols + c `mod` cols
+
+  rows = (n + cols - 1) `div` cols
+  cols = 4
+
+printGame :: Vty -> Interface -> IO Interface
+printGame vty s = do
+   update vty $ make_picture
+              $ interfaceImage s
+   case iTimer s of
+     Nothing -> return s
+     Just (delay, s') -> do
+       threadDelay delay
+       printGame vty s'
+
+-- | 'giveHint' checks for a hint in the current game and alters the
+--   selection if a hint is found.
+giveHint :: Game -> Interface -> Interface
+giveHint game s = setGen g . f $ s
+  where
+  f = case mbHint of
+        Just a -> setMessage hintmsg . setSelection [a]
+        Nothing -> setMessage dealmsg
+
+  (mbHint, g) = hint (iStdGen s) game
+
+  hintmsg = "There is a set using this card."
+  dealmsg = "No sets in this tableau, deal more cards."
+
+-- | 'checkSet' will extract the chosen set from the tableau and check it
+--   for validity. If a valid set is removed from the tableau the tableau
+--   will be refilled up to 12 cards.
+checkSet :: Card -> Card -> Card -> Game -> Interface
+         -> Maybe (Game, Interface)
+checkSet a b c game s = Just $ case considerSet a b c game of
+  Nothing       -> (game, setMessage "Not a set!"
+                        . delayedUpdate (seconds 1 `div` 4)
+                        ( setMessage "Not a set.")
+                        $ s)
+
+  Just game'    -> (game', delayedUpdate (seconds 1)
+                        ( setGame game'
+                        . clearSelection
+                        . setMessage "Good job.")
+                        . setMessage "Good job!"
+                        $ s)
+
+-- | 'checkNoSets' verifies that there are no sets in the current tableau
+-- and deals additional cards to the tableau in that case.
+checkNoSets :: Game -> Interface -> Maybe (Game, Interface)
+checkNoSets game s = case extraCards game of
+  Right game' -> Just (game', setGame game'
+                            . incDealCounter
+                            . setMessage "Dealing more cards."
+                            $ s)
+  Left 0 -> Nothing
+  Left sets -> Just (game, incBadDealCounter
+                         . setMessage msg
+                         $ s)
+    where
+       msg = "Oops, " ++ show sets ++ " sets in tableau. Keep looking."
+
+-------------------------------------------------------------------------------
+-- Interface manipulation functions -------------------------------------------
+-------------------------------------------------------------------------------
 
 data Interface = IS
   { iControl :: CurrentControl
@@ -101,10 +191,6 @@ setTableau xs s = s { iTableau = xs }
 
 setTimer :: Int -> Interface -> Interface -> Interface
 setTimer delay s' s = s { iTimer = Just (delay, s') }
-
-tempUpdate :: Int -> (Interface -> Interface)
-                  -> Interface -> Interface
-tempUpdate delay f s = setTimer delay s (f s)
 
 delayedUpdate :: Int -> (Interface -> Interface)
               -> Interface -> Interface
@@ -150,7 +236,9 @@ isSelected x i = x `elem` iSelection i
 setGen :: StdGen -> Interface -> Interface
 setGen g i = i { iStdGen = g }
 
--- Input loop and types
+-------------------------------------------------------------------------------
+-- Input to even mapping ------------------------------------------------------
+-------------------------------------------------------------------------------
 
 data Command = Move Direction | Select | Quit | Deal | DeleteLast | Hint
 data Direction = GoUp | GoDown | GoLeft | GoRight
@@ -170,69 +258,6 @@ handleInput vty = do
    EvKey (KASCII 'q') [] -> return Quit
    _ -> handleInput vty
 
-moveCur :: Direction -> Int -> CurrentControl -> CurrentControl
-moveCur dir n (CardButton c) = correct $ case dir of
-  GoUp -> c - width
-  GoDown -> c + width
-  GoLeft -> c - 1
-  GoRight -> c + 1
- where
- width = 4
- correct i = CardButton (i `mod` n)
-
-printGame :: Vty -> Interface -> IO Interface
-printGame vty s = do
-   update vty $ make_picture
-              $ interfaceImage s
-   case iTimer s of
-     Nothing -> return s
-     Just (delay, s') -> do
-       threadDelay delay
-       printGame vty s'
-
-giveHint :: Game -> Interface -> Interface
-giveHint game s = setGen g . f $ s
-  where
-  f = case mbHint of
-        Just a -> setMessage hintmsg . setSelection [a]
-        Nothing -> setMessage dealmsg
-
-  (mbHint, g) = hint (iStdGen s) game
-
-  hintmsg = "There is a set using this card."
-  dealmsg = "No sets in this tableau, deal more cards."
-
--- | 'checkSet' will extract the chosen set from the tableau and check it
---   for validity. If a valid set is removed from the tableau the tableau
---   will be refilled up to 12 cards.
-checkSet :: Card -> Card -> Card -> Game -> Interface
-         -> (Game, Interface)
-checkSet a b c game s = case considerSet a b c game of
-  Nothing       -> (game, setMessage "Not a set!"
-                        . delayedUpdate (seconds 1 `div` 4)
-                        ( setMessage "Not a set.")
-                        $ s)
-
-  Just game'    -> (game', setMessage "Good job!"
-                        . delayedUpdate (seconds 1)
-                        ( setGame game'
-                        . clearSelection
-                        . setMessage "Good job.")
-                        $ s)
-
-checkNoSets :: Game -> Interface -> Maybe (Game, Interface)
-checkNoSets game s = case extraCards game of
-  Right game' -> Just (game', setGame game'
-                            . incDealCounter
-                            . setMessage "Dealing more cards."
-                            $ s)
-  Left 0 -> Nothing
-  Left sets -> Just (game, incBadDealCounter
-                         . setMessage msg
-                         $ s)
-    where
-       msg = "Oops, " ++ show sets ++ " sets in tableau. Keep looking."
-
 -------------------------------------------------------------------------------
 -- Interface rendering functions ----------------------------------------------
 -------------------------------------------------------------------------------
@@ -250,7 +275,7 @@ interfaceImage s =
   <->
   plainString helpString
   <->
-  vert_cat (map cardRow rows)
+  tableauImage s
   <->
   plainString "Cards remaining in deck: "
     <|> boldString  (leftPadText 2 (show (iRemaining s)))
@@ -265,33 +290,44 @@ interfaceImage s =
   <->
   plainString "Current Selection"
   <->
-  cardRow (map ((,,) False False) (iSelection s))
-  where
-  plainString = string def_attr
-  boldString = string (def_attr `with_style` bold)
-  rows = groups 4 (zipWith testFocus [0..] (iTableau s))
-  testFocus i c = (iControl s == CardButton i, isSelected c s, c)
-  cardRow = horiz_cat . map cardimage
+  cardRowImage (map ((,,) False False) (iSelection s))
 
-cardimage :: (Bool, Bool, Card) -> Image
-cardimage (focused,selected,c)
-     = char_fill fill_attr left_filler 1 (4 :: Int)
-   <|> (vert_cat (map (Vty.string card_attr) xs))
-   <|> char_fill fill_attr right_filler 1 (4 :: Int)
-   <-> char_fill def_attr ' ' 18 (1 :: Int)
+tableauImage :: Interface -> Image
+tableauImage s = vert_cat
+               $ map cardRowImage
+               $ groups 4 
+               $ zipWith testFocus [0..] 
+               $ iTableau s
+  where
+  testFocus i c = (iControl s == CardButton i, isSelected c s, c)
+
+cardRowImage :: [(Bool, Bool, Card)] -> Image
+cardRowImage = horiz_cat . map cardImage
+
+-- | 'cardImage' renders a 'Card' based on its current selection and focus
+--   state.
+cardImage :: (Bool, Bool, Card) -> Image
+cardImage (focused,selected,c) = left_side <|> body <|> right_side <-> bottom
  where
+  body          = vert_cat (map (Vty.string card_attr) xs)
+  left_side     = char_fill fill_attr left_filler 1 (4 :: Int)
+  right_side    = char_fill fill_attr right_filler 1 (4 :: Int)
+  bottom        = char_fill def_attr ' ' 18 (1 :: Int)
+
   (card_color, xs) = cardLines c
+
   vty_color = case card_color of
-    Red -> red
-    Purple -> cyan
-    Green -> green
+    Red         -> red
+    Purple      -> cyan
+    Green       -> green
 
   (fill_attr, left_filler, right_filler)
     | focused   = (def_attr`with_fore_color`white`with_back_color`yellow,
                      '▶', '◀')
     | otherwise = (def_attr, ' ', ' ')
+
   card_attr
-    | selected = base_card_attr `with_style` reverse_video
+    | selected  = base_card_attr `with_style` reverse_video
     | otherwise = base_card_attr
 
   base_card_attr = def_attr `with_fore_color` vty_color `with_back_color` black
@@ -299,3 +335,9 @@ cardimage (focused,selected,c)
 make_picture :: Image -> Picture
 make_picture img = (pic_for_image img)
  { pic_background = Background ' ' def_attr }
+
+plainString :: String -> Image
+plainString = string def_attr
+
+boldString :: String -> Image
+boldString = string (def_attr `with_style` bold)
