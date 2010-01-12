a63a2ad6b9e04c5aeb0ba57f35244394c4645d9b
Eric Mertens <emertens@eric-mertenss-macbook-pro.local>
2010-01-11 20:01:40 -0800

    Remove the small dependency on monadlib

--- src/Set/Utils.hs
+++ src/Set/Utils.hs
@@ -2,7 +2,6 @@ module Set.Utils where
 
 import Control.Monad                    (liftM)
 import Data.List			(tails)
-import MonadLib                         (StateM(..), runId, runStateT)
 import System.Random                    (newStdGen, RandomGen, randomR)
 
 -------------------------------------------------------------------------------
@@ -18,6 +17,8 @@ groups n xs = as : groups n bs
   where
   (as,bs) = splitAt n xs
 
+-- | 'delete1' returns a list with the first occurrence of @x removed. If there
+-- is no occurrence 'Nothing' is returned.
 delete1 :: Eq a => a -> [a] -> Maybe [a]
 delete1 x (y:ys)
   | x == y = Just ys
@@ -31,27 +32,12 @@ index 0 (x:_)		= Just x
 index n (_:xs) | n > 0	= index (n-1) xs
 index _ _		= Nothing
 
-uniques :: Eq a => [a] -> Bool
-uniques = all (uncurry (/=)) . chooseTwo
-
 select :: Int -> [a] -> (a,[a])
 select _ [] = error "select: index too large"
 select 0 (x:xs) = (x,xs)
 select n (x:xs) = (y,x:ys)
   where
   (y,ys) = select (n-1) xs
-
-select3 :: Int -> Int -> Int -> [a] -> (a,a,a,[a])
-select3 a b c xs = (x0,x1,x2,xs2)
-  where
-  (x0,xs0) = select a xs
-  (x1,xs1) = select (dec a b b) xs0
-  (x2,xs2) = select (dec a c (dec b c c)) xs1
-
-  -- | 'dec' is used to correct indexes that will be affected
-  -- by previous selects.
-  dec x y z | x < y     = z - 1
-            | otherwise = z
 
 -- | Drop last element of list if there is an element to drop.
 init' :: [a] -> [a]
@@ -61,10 +47,6 @@ init' xs = init xs
 -------------------------------------------------------------------------------
 -- Other utilities ------------------------------------------------------------
 -------------------------------------------------------------------------------
-
--- | 'bounded' test that x is between lo and hi (inclusive).
-bounded :: Ord a => a -> a -> a -> Bool
-bounded lo hi x = lo <= x && x <= hi
 
 -- | 'chooseThree' returns all combinations of three elements.
 chooseTwo :: [a] -> [(a,a)]
@@ -86,23 +68,16 @@ shuffleIO xs = liftM (fst . shuffle xs) newStdGen
 
 -- | 'shuffle' shuffles the elements of a list using the given random generator.
 shuffle :: RandomGen g => [a] -> g -> ([a], g)
-shuffle xs g = runId (runStateT g (shuffle' (length xs) xs))
+shuffle xs = shuffle' (length xs) xs
 
-shuffle' :: (RandomGen g , StateM m g) =>  Int -> [a] -> m [a]
-shuffle' _ [] = return []
-shuffle' n xs = do
-  let n' = n - 1
-  i <- getRandom n'
-  let (x, xs') = select i xs
-  xs'' <- shuffle' n' xs'
-  return (x:xs'')
-
-getRandom :: (RandomGen g, StateM m g) => Int -> m Int
-getRandom b = do
-  (i, g') <- randomR (0,b) `liftM` get
-  set g'
-  return i
-
+shuffle' :: RandomGen g =>  Int -> [a] -> g -> ([a], g)
+shuffle' _ [] g = ([], g)
+shuffle' n xs g = (x:xs'', g'')
+  where
+  n'		= n - 1
+  (i, g')	= randomR (0,n') g
+  (x, xs')	= select i xs
+  (xs'', g'')	= shuffle' n' xs' g'
 
 -------------------------------------------------------------------------------
 -- Text manipulation utilities ------------------------------------------------
